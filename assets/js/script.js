4cd6506d20bcdbda56a75c1d107c71ab4494eee4
Zach Yarbrough <zachyarbro@gmail.com>
2021-07-11 18:23:25 -0500

    add function to create tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,21 @@
-var formEl = document.querySelector("#task-form")
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function (event) {
-    event.preventDefault();
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
-    var taskItemEl = document.createElement("li");
+  var listItemEl = document.createElement("li");
+  listItemEl.className = "task-item";
 
-    taskItemEl.textContent = "New List Element";
-    taskItemEl.className = "task-item";
-    tasksToDoEl.appendChild(taskItemEl);
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
 
-    console.log(event);
-}
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  tasksToDoEl.appendChild(listItemEl);
+};
 
 formEl.addEventListener("submit", createTaskHandler);
